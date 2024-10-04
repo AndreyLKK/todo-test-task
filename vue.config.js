a8cc6da9d8a733f7ccf8435899a3d0c01ccc53a4
AndreyLKK <awesomestim@gmail.com>
2024-10-04 14:27:27 +0700

    change folder name

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
-  base: "/todo-test-task/",
+  publicPath: "/todo-test-task/",
   lintOnSave: false,
   css: {
     loaderOptions: {
